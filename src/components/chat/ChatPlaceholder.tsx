7829a9d794dbe26902a57e1a62feedaf277bcc3c
Ricky Robinett <richard.robinett@gmail.com>
2023-09-26 16:58:45 -0400

    update placeholder

--- src/components/chat/ChatPlaceholder.tsx
+++ src/components/chat/ChatPlaceholder.tsx
@@ -8,7 +8,7 @@ export default function ChatPlaceholder({}: Props) {
       <div className="max-w-3xl p-4 text-center text-primary">
         <h1 className="text-4xl font-medium">ChatCloudflare</h1>
         <p className="mt-4 text-lg">
-          A ChatGPT clone built with React, Next.js, TailwindCSS, and Cloudflare Workers AI.
+          A Chat app built with <a href="https://react.dev/">React</a>, <a href="https://nextjs.org/">Next.js</a>, <a href="https://tailwindcss.com/">Tailwind CSS</a>, and <a href="https://ai.cloudflare.com">Cloudflare Workers AI</a>.
         </p>
         <p className="p-6">Based on <a href="https://github.com/Nashex/gpt4-playground">GPT-4 Playground by Nashex</a></p>
       </div>
